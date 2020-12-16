d1ac22b83d8464128ee2b4265096f9c1c5cce3bc
Dmitry Sterlin <uglenpish@rambler.ru>
2020-12-16 16:36:45 +0300

    реализовал функцию mySortForKey

--- 2.php
+++ 2.php
@@ -24,3 +24,30 @@ $result = convertString($str, $last);
 print_r($result);
 
 echo "<br>";
+
+/*- функию mySortForKey($a, $b). $a – двумерный массив вида [['a'=>2,'b'=>1],['a'=>1,'b'=>3]], $b – ключ вложенного массива. Результат ее выполнения: двумерном массива $a отсортированный по возрастанию значений для ключа $b. В случае отсутствия ключа $b в одном из вложенных массивов, выбросить ошибку класса Exception с индексом неправильного массива.*/
+
+function mySortForKey($a, $b)
+{
+	$sortArr = [];
+  foreach($a as $key=>$val){
+  	if(!array_key_exists($b, $val)){
+  		throw new Exception("Массив с индексом $key не содержит ключь $b");
+  	}
+    $sortArr[$key] = $val[$b];
+  }
+
+  array_multisort($sortArr, $a);
+
+  return $a;
+}
+
+$arr = [['a'=>2,'b'=>1], ['a'=>1,'b'=>3], ['a'=>7,'b'=>2]];
+$key = 'a';
+
+print_r(mySortForKey($arr, $key));
+
+$arr = [['a'=>2,'b'=>1], ['b'=>3], ['a'=>7,'b'=>2]];
+$key = 'a';
+
+print_r(mySortForKey($arr, $key));
